3bde0e1980c5f9c6d9311a55fe1d74d4cea304ba
Prince Sharma <hit.princeraj@gmail.com>
2025-09-20 09:25:35 +0530

    add env

---
+++ backend/src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+function connectDB() {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => {
+      console.error("MongoDB connection error:", err);
+      process.exit(1);
+    });
+}
+
+
+export default connectDB;
