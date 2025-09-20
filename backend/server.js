801452803d1207adc89e4fa58f769f64a319f47f
Prince Sharma <hit.princeraj@gmail.com>
2025-09-20 09:03:35 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import app from '../backend/src/app.js'
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("server start on port num 3000");
+
+})
---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express
+
+
+
+
+
+
+
+
+
+
+
+express default app
